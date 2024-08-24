6343c2961501e2d4abb0bcee1560bf998c7a29fd
horacio1986 <horaciomontano31@gmail.com>
2024-08-23 23:54:15 -0300

    UPD: manifest to 17.0 version

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Sales",
-    "version": "16.0.0.0.0",
+    "version": "0.1",
     # any module necessary for this one to work correctly
     "depends": ["sale", "product"],
     # always loaded
